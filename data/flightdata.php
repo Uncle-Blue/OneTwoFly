9ab934ae162be52e2556ab10429cfc38894248a8
wsxedcrfv210 <sapc87952@gmail.com>
2017-04-30 07:51:25 +0800

    修改request內容
    bug fix

--- data/flightdata.php
+++ data/flightdata.php
@@ -14,26 +14,45 @@ $options = array(
         $params = array("ident" => "flightnum", "howMany" => "1");
         $result2ports = $client->FlightInfo($params);
         
+        //起點機場代號
         $origin = $result2ports->FlightInfoResult->flights->origin;
+        //終點機場代號
         $destination = $result2ports->FlightInfoResult->flights->destination;
 
         print_r($result2ports);
 
-
+        
         $params_ori = array("airportCode" => "$origin");
+
+        
         $params_des = array("airportCode" => "$destination");
 
         $result_ori = $client->AirportInfo($params_ori);
         $result_des = $client->AirportInfo($params_des);
 
-        print_r($result_ori);
-        print_r($result_des);
+       
+        
 
-        $ori_lon = $result_ori->AirportInfoResult->longitude;
-        $ori_lat = $result_ori->AirportInfoResult->latitude;
+        //起點經緯度*
+        $origin_geo = json_encode(result_ori);
+        print(origin_geo);
+        //終點經緯度*
+        $destination_geo = json_encode(result_des);
+        print(destination_geo);
 
-        $des_lon = $result_des->AirportInfoResult->longitude;
-        $des_lat = $result_des->AirportInfoResult->latitude;
+        //起點氣象
+        $params_ori_weather = array("airportCode" => $origin, "startTime" => 0, "howMany" => 1, "offset" => 0)
+        $weather_ori = $client->MetarEx($params_ori_weather);
+        
+        //起點氣象報告*
+        $weather_report_ori = json_encode($weather_ori);
+
+        //終點氣象
+        $params_des_weather = array("airportCode" => $destination, "startTime" => 0, "howMany" => 1, "offset" => 0)
+        $weather_des = $client->MetarEx($params_ori_weather);
+        
+        //終點氣象報告*
+        $weather_report_des = json_encode($weather_des);
 
 
 
